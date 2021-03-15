d49a27fc233571ff8dace9f5e1bc84bf1cbc18fc
Zydj <charles.desjardins96@gmail.com>
2021-03-15 16:50:47 -0400

    Created the player and playercontroller classes
    Created the classes needed to handle the player data during the execution of the mod.

---
+++ Sheriff/Sheriff/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sheriff
+{
+    public class Player
+    {
+        public PlayerControl playerdata;
+        public List<string> components;
+        public int PlayerId;
+
+        public Player(PlayerControl playerdata)
+        {
+            this.playerdata = playerdata;
+            components = new List<string>();
+            this.PlayerId = playerdata.PlayerId;
+        }
+
+        public bool hasComponent(String name)
+        {
+            if (components.Contains(name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Sheriff/Sheriff/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+
+namespace Sheriff
+{
+    public static class PlayerController
+    {
+        public static List<Player> players;
+        public static Player LocalPlayer;
+
+        public static void InitPlayers()
+        {
+            players = new List<Player>();
+
+            foreach (PlayerControl player in PlayerControl.AllPlayerControls)
+            {
+                Player p = new Player(player);
+                if (player.PlayerId == PlayerControl.LocalPlayer.PlayerId)
+                {
+                    LocalPlayer = p;
+                }
+                players.Add(p);
+            }
+        }
+
+        public static Player getPlayerById(byte id)
+        {
+            if (players == null) return null;
+
+            foreach (Player player in players)
+            {
+                if (player.PlayerId == id)
+                {
+                    return player;
+                }
+            }
+
+            return null;
+        }
+
+        public static Player getLocalPlayer()
+        {
+            if (players == null) return null;
+            foreach (Player player in players)
+            {
+                if (player.playerdata.PlayerId == PlayerControl.LocalPlayer.PlayerId)
+                {
+                    return player;
+                }
+            }
+            return null;
+        }
+
+        public static Player getPlayerByRole(string role)
+        {
+            foreach (Player player in players)
+            {
+                if (player.hasComponent(role))
+                {
+                    return player;
+                }
+            }
+            return null;
+        }
+
+        public static PlayerControl getPlayerControlByRole(string role)
+        {
+            Player player = getPlayerByRole(role);
+            foreach (PlayerControl playerCon in PlayerControl.AllPlayerControls)
+            {
+                if (playerCon.PlayerId == player.PlayerId)
+                {
+                    return playerCon;
+                }
+            }
+
+            return null;
+        }
+
+        public static PlayerControl getPlayerControlById(byte id)
+        {
+            foreach (PlayerControl playerCon in PlayerControl.AllPlayerControls)
+            {
+                if (playerCon.PlayerId == id)
+                {
+                    return playerCon;
+                }
+            }
+            return null;
+        }
+    }
+}
