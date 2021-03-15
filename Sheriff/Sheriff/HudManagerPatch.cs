bac2365f221a59ffd9efad8ed2e41904adeb3d5a
Zydj <charles.desjardins96@gmail.com>
2021-03-15 17:06:35 -0400

    Added a reminder for the sheriff role
    Added the code in to show a reminder for the player with the sheriff role next to their tasks.

---
+++ Sheriff/Sheriff/HudManagerPatch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using InnerNet;
+
+namespace Sheriff
+{
+    [HarmonyPatch]
+    public static class HudManagerPatch
+    {
+        [HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
+        static void Postfix(HudManager __instance)
+        {
+            if (!Sheriff.sheriffEnabled)
+            {
+                return;
+            }
+
+            if (AmongUsClient.Instance.GameState != InnerNetClient.Nested_0.Started)
+                return;
+
+            if (!Sheriff.sheriffEnabled)
+            {
+                return;
+            }
+
+            if (PlayerController.LocalPlayer.hasComponent("Sheriff"))
+            {
+                string currentTasks = __instance.TaskText.Text;
+
+                currentTasks = currentTasks.Replace("[FFB400FF]Kill the imposter.\n[FFFFFFFF]", "");
+                __instance.TaskText.Text = "[FFB400FF]Kill the imposter.\n[FFFFFFFF]" + currentTasks;
+            }
+        }
+    }
+}
