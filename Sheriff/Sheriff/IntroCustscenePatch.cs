a2331d77505538e5a7a096045d1f616963da7942
Zydj <charles.desjardins96@gmail.com>
2021-03-15 16:52:13 -0400

    Created the intro patch file
    Added the code file to handle the sheriff intro if the player is sheriff.

---
+++ Sheriff/Sheriff/IntroCustscenePatch.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+
+namespace Sheriff
+{
+    [HarmonyPatch]
+    public static class IntroCustscenePatch
+    {
+        [HarmonyPatch(typeof(IntroCutscene.Nested_0), nameof(IntroCutscene.Nested_0.MoveNext))]
+        static void Prefix(IntroCutscene.Nested_0 __instance)
+        {
+            Sheriff.introDone = false;
+        }
+
+        [HarmonyPatch(typeof(IntroCutscene.Nested_0), nameof(IntroCutscene.Nested_0.MoveNext))]
+        public static void Postfix(IntroCutscene.Nested_0 __instance)
+        {
+            if (!Sheriff.sheriffEnabled)
+            {
+                return;
+            }
+
+            if (PlayerController.getLocalPlayer().hasComponent("Sheriff"))
+            {
+                if (Sheriff.debug)
+                {
+                    Sheriff.log.LogMessage("Setting sheriff intro");
+                }
+
+                __instance.__this.Title.Text = "Sheriff";
+                __instance.__this.Title.Color = Sheriff.sheriffColor;
+                __instance.__this.ImpostorText.Text = "Kill the [FF0000FF]Impostor";
+                __instance.__this.BackgroundBar.material.color = Sheriff.sheriffColor;
+            }
+
+            Sheriff.introDone = true;
+        }
+    }
+}
