616902e62d753f7de90e803f47ffcd6de810ebce
Zydj <charles.desjardins96@gmail.com>
2021-03-15 16:51:29 -0400

    Created the RPC file
    Created the RPC file to handle the important initial RPCs and added the ones needed for the sheriff mod.

---
+++ Sheriff/Sheriff/RPC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sheriff
+{
+    enum RPC
+    {
+        CompleteTask = 1
+    }
+    enum CustomRPC
+    {
+        SetJester = 40,
+        JesterWin = 50,
+        SetLocalPlayersJester = 41,
+        SyncCustomSettingsJester = 42,
+        SetLastPlayerTask = 43,
+        SetSheriff = 44,
+        SetLocalPlayersSheriff = 45
+    }
+}
