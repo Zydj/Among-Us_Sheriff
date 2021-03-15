769218050be22b66c3d15f3e9d1024753597881e
Zydj <charles.desjardins96@gmail.com>
2021-03-15 16:49:42 -0400

    Set up the plugin file
    Renamed the plugin file to a fitting name and populated it with inital variables.

---
+++ Sheriff/Sheriff/Sheriff.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using System.Collections.Generic;
+using Reactor;
+using UnityEngine;
+
+namespace Sheriff
+{
+    [BepInPlugin(Id)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class Sheriff : BasePlugin
+    {
+        public static bool debug = true;
+
+        public const string Id = "org.bepinex.plugins.Sheriff";
+        public static BepInEx.Logging.ManualLogSource log;
+        
+        public static bool sheriffEnabled = true;
+        public static bool introDone= false;
+        
+
+        public static PlayerControl localPlayer = null;
+        public static List<PlayerControl> localPlayers = new List<PlayerControl>();
+
+        public static Color sheriffColor = new Color(1, (float)(180.0 / 255.0), 0);
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+        public ConfigEntry<string> Name { get; private set; }
+
+        public override void Load()
+        {
+            log = Log;
+            log.LogMessage("Sheriff Mod Loaded");
+
+            Harmony.PatchAll();
+        }        
+    }
+}
