ba218c5ebadda048f26899f6a824dad2a81ba7d3
Zydj <charles.desjardins96@gmail.com>
2021-03-17 02:10:07 -0400

    Added the code to show the new options in the side bar options

---
+++ Sheriff/Sheriff/GameOptionsDataPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace Sheriff
+{
+    [HarmonyPatch]
+    public class GameOptionsDataPatch
+    {
+        [HarmonyPatch(typeof(GameOptionsData), nameof(GameOptionsData.Method_5))]
+        public static void Postfix(ref string __result)
+        {
+            if (Sheriff.sheriffEnabled)
+            {
+                __result += "Show Sheriff: On\n";
+            }
+            else
+            {
+                __result += "Show Sheriff: Off\n";
+            }
+
+            __result += "Sheriff Kill Cooldown: " + Sheriff.sheriffKillCooldown.ToString() + "s";
+        }
+    }
+}
