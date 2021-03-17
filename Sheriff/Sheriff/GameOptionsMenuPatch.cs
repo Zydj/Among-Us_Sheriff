acb16c79aef9801dee34ac448c4a97b82f85f26c
Zydj <charles.desjardins96@gmail.com>
2021-03-17 16:27:23 -0400

    Added the code to show the classic sheriff option in the menu

--- Sheriff/Sheriff/GameOptionsMenuPatch.cs
+++ Sheriff/Sheriff/GameOptionsMenuPatch.cs
@@ -8,15 +8,21 @@ namespace Sheriff
     {
         public static ToggleOption showSheriffOption;
         public static NumberOption sheriffKillCooldown;
+        public static ToggleOption showClassicSheriffOption;
+
         public static GameOptionsMenu instance;
 
-        [HarmonyPatch(typeof(GameOptionsMenu), nameof(GameOptionsMenu.Start))]
+        public static float defaultBounds;
+
+        [HarmonyPatch(typeof(GameOptionsMenu), nameof(GameOptionsMenu.Start))]        
         public static void Postfix(GameOptionsMenu __instance)
         {
             if (Sheriff.debug)
             {
                 Sheriff.log.LogMessage("Starting");
             }
+
+            defaultBounds = __instance.GetComponentInParent<Scroller>().YBounds.max;
 
             instance = __instance;
             CustomPlayerMenuPatch.AddOptions();
@@ -26,12 +32,15 @@ namespace Sheriff
         [HarmonyPatch(typeof(GameOptionsMenu), nameof(GameOptionsMenu.Update))]
         public static void Postfix1(GameOptionsMenu __instance)
         {
-            OptionBehaviour option = __instance.Children[__instance.Children.Count - 3];            
+            OptionBehaviour option = __instance.Children[__instance.Children.Count - 4];
 
-            if (showSheriffOption != null && sheriffKillCooldown != null)
+            if (showSheriffOption != null && sheriffKillCooldown != null && showClassicSheriffOption != null)
             {
                 showSheriffOption.transform.position = option.transform.position - new Vector3(0, 0.5f, 0);
                 sheriffKillCooldown.transform.position = option.transform.position - new Vector3(0, 1f, 0);
+                showClassicSheriffOption.transform.position = option.transform.position - new Vector3(0, 1.5f, 0);                
+
+                __instance.GetComponentInParent<Scroller>().YBounds.max = defaultBounds + 1.5f;
             }
         }
     }
