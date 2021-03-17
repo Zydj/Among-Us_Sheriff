a013140bf4d1f7dc421c6bb0133a38daad4dc921
Zydj <charles.desjardins96@gmail.com>
2021-03-17 02:12:04 -0400

    Added the code to handle the toggling of the sheriff role

---
+++ Sheriff/Sheriff/ToggleButtonPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace Sheriff
+{
+    [HarmonyPatch]
+    public static class ToggleButtonPatch
+    {
+        [HarmonyPatch(typeof(ToggleOption), nameof(ToggleOption.Toggle))]
+        public static bool Prefix(ToggleOption __instance)
+        {
+            if (__instance.TitleText.Text == "Sheriff Role")
+            {
+                Sheriff.sheriffEnabled = !Sheriff.sheriffEnabled;
+                PlayerControl.LocalPlayer.RpcSyncSettings(PlayerControl.GameOptions);
+
+                __instance.oldValue = Sheriff.sheriffEnabled;
+                __instance.CheckMark.enabled = Sheriff.sheriffEnabled;
+
+                return false;
+            }
+            return true;
+        }
+    }
+}
