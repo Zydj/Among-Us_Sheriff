444e69a8adec7312d69ce7e123c614799f5630c9
Zydj <charles.desjardins96@gmail.com>
2021-03-17 21:36:15 -0400

    Fixed the code for the compatibility
    Changed where we get the position of last component to use as a basis for the position of the new components.

--- Sheriff/Sheriff/GameOptionsMenuPatch.cs
+++ Sheriff/Sheriff/GameOptionsMenuPatch.cs
@@ -9,6 +9,8 @@ namespace Sheriff
         public static ToggleOption showSheriffOption;
         public static NumberOption sheriffKillCooldown;
         public static ToggleOption showClassicSheriffOption;
+        
+        public static OptionBehaviour option;
 
         public static GameOptionsMenu instance;
 
@@ -22,9 +24,12 @@ namespace Sheriff
                 Sheriff.log.LogMessage("Starting");
             }
 
+            instance = __instance;
+
             defaultBounds = __instance.GetComponentInParent<Scroller>().YBounds.max;
 
-            instance = __instance;
+            option = __instance.Children[__instance.Children.Count-1];
+
             CustomPlayerMenuPatch.AddOptions();
         }
 
@@ -32,15 +37,13 @@ namespace Sheriff
         [HarmonyPatch(typeof(GameOptionsMenu), nameof(GameOptionsMenu.Update))]
         public static void Postfix1(GameOptionsMenu __instance)
         {
-            OptionBehaviour option = __instance.Children[__instance.Children.Count - 4];
-
             if (showSheriffOption != null && sheriffKillCooldown != null && showClassicSheriffOption != null)
             {
                 showSheriffOption.transform.position = option.transform.position - new Vector3(0, 0.5f, 0);
                 sheriffKillCooldown.transform.position = option.transform.position - new Vector3(0, 1f, 0);
                 showClassicSheriffOption.transform.position = option.transform.position - new Vector3(0, 1.5f, 0);
 
-                __instance.GetComponentInParent<Scroller>().YBounds.max = defaultBounds + 0.5f;
+                __instance.GetComponentInParent<Scroller>().YBounds.max = defaultBounds + 1.5f;
             }
         }
     }
